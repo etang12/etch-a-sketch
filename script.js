fe6059c88dfea720f485c061867321cce27e9948
Eric Tang <etang12@ucsc.edu>
2022-10-02 23:07:13 -0700

    implemented color-in effect while hovering and reset grid

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
+
 const grid_container = document.getElementById("grid-container");
+const reset_button = document.getElementsByClassName("reset-button")[0];
+
+const colorIn = (e) => {
+    e.target.classList.add('color-in');
+}
+
+const resetGrid = () => {
+    console.log('inside reset');
+    const all_grid_items = document.querySelectorAll('.grid-item');
+    console.log(all_grid_items);
+    all_grid_items.forEach((item) => {
+        item.classList.remove('color-in');
+    });
+};
+
 
 const createGrid = (rows, cols) => {
     grid_container.style.setProperty('--grid-rows', rows);
@@ -14,3 +30,8 @@ const createGrid = (rows, cols) => {
 
 createGrid(16, 16);
 
+grid_container.addEventListener("mouseover", (event) => {
+    colorIn(event);
+});
+
+reset_button.addEventListener("click", resetGrid);
