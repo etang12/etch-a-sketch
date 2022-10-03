a6561cf9deba5738fdebdf34460400e31f1e15ce
Eric Tang <etang12@ucsc.edu>
2022-10-03 01:35:56 -0700

    added slider and ability to change grid size

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 
 const grid_container = document.getElementById("grid-container");
 const reset_button = document.getElementsByClassName("reset-button")[0];
+const slider = document.getElementById("slider-range");
+
+let mouse_down = false;
+
+document.body.onmousedown = () => {
+    mouse_down = true;
+}
+
+document.body.onmouseup = () => {
+    mouse_down = false;
+}
+
+const changeGridSize = (range) => {
+    document.getElementsByClassName('range-output')[0].textContent = `${range.value}x${range.value}`;
+    clearGrid();
+    createGrid(range.value, range.value);
+};
 
 const colorIn = (e) => {
+    if (e.type === "mouseover" && !mouse_down) {
+        return;
+    };
+    
     e.target.classList.add('color-in');
 }
 
-const resetGrid = () => {
+const resetGridColors = () => {
     console.log('inside reset');
     const all_grid_items = document.querySelectorAll('.grid-item');
     console.log(all_grid_items);
@@ -22,16 +43,17 @@ const createGrid = (rows, cols) => {
 
     for (i = 0; i < rows * cols; i++) {
         let grid_item = document.createElement('div');
-        grid_item.innerText = (i + 1);
         grid_item.classList.add('grid-item');
+        grid_item.addEventListener("mousedown", colorIn);
+        grid_item.addEventListener("mouseover", colorIn);
         grid_container.appendChild(grid_item);
     };
 };
 
+const clearGrid = () => {
+    grid_container.innerHTML = '';
+}
+
 createGrid(16, 16);
 
-grid_container.addEventListener("mouseover", (event) => {
-    colorIn(event);
-});
-
-reset_button.addEventListener("click", resetGrid);
+reset_button.addEventListener("click", resetGridColors);
